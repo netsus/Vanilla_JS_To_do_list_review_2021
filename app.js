8a03fc01d1a80d9124d206ae7fde3ff99a0b1c3c
netsus <57648890+netsus@users.noreply.github.com>
2021-07-04 20:19:39 +0900

    #3.3 Events - EventListener
    addEventListener( [특정 event]  ,  [event가 발생하면 호출할 function] )
    ex) title 클릭하면 handleTitleClick함수 실행
    title.addEventListener("click", handleTitleClick);

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const title = document.querySelector(".hello h1:first-child")
 
-console.dir(title);
 
-title.style.color = "blue";
+function handleTitleClick() {
+    console.log("title was clicked!");
+}
+
+function changeColor() {
+    title.style.color = "blue"; // title 클릭 -> 파란색으로 변경
+}
+
+title.addEventListener("click", handleTitleClick);
+
+title.addEventListener("click", changeColor);
