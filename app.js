afcd0f337a4fafec837d52edcd176f4b670bf1b0
netsus <57648890+netsus@users.noreply.github.com>
2021-07-11 22:04:38 +0900

    #3.7 CSS in Javascript part Two - Js className
    h1을 클릭할 때마다 색깔이 바뀌게 한다. - CSS의 클래스 이름을 이용해서
    h1.className이 "clicked"이면 클래스 이름을 빈 문자열로 하고, 아니면 "clicked"클래스이름 주기
    --> 클릭할 때마다 클래스가 바뀌어 색도 바뀌게함 (CSS의 클래스이름을 통해서)
        if(h1.className ===  "clicked"){
            h1.className = "";
        }else{
            h1.className =  "clicked";
        }
    ** 기존의 클래스를 덮어쓰는 버그 존재 --> 다음시간에 이 부분 해결해보기

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const h1 = document.querySelector(".hello h1:first-child")
 
 
 function handleTitleClick(event) {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue"){
-        newColor = "tomato";
-    }else {
-        newColor = "blue";
+    const clickedClass = "clicked";
+    if(h1.className === clickedClass){
+        h1.className = "";
+    }else{
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click",handleTitleClick);
