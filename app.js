280551062faba2adfce0a4cffde814a4f725866a
netsus <57648890+netsus@users.noreply.github.com>
2021-07-11 22:19:42 +0900

    #3.8 CSS in Javascript part Three - toggle
    toggle : 클래스가 해당 엘레멘트에 있으면 삭제, 없으면 추가하는 함수 (if, else
    ex) tg.classList.toggle("clicked");  --> classList에 "clicked"가 있으면 삭제하고, 없으면 추가하기
    --> 특정 이벤트에 대해 toggle 가능

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector(".hello h1:first-child")
 
 
 function handleTitleClick(event) {
-    const clickedClass = "clicked";
-    if(h1.className === clickedClass){
-        h1.className = "";
-    }else{
-        h1.className = clickedClass;
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click",handleTitleClick);
