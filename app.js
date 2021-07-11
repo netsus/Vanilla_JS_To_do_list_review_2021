55cd014e61e12c052aa53411f8664b5b82fbcdb8
netsus <57648890+netsus@users.noreply.github.com>
2021-07-11 21:19:24 +0900

    #3.5 More Events
    1. 이벤트처리 방법 2가지 - 동일한 이벤트 처리
      1) title.onclick = handleTitleClick;
      2) title.addEventListener("click",handleTitleClick);
    
    2. document에서 직접 가져올 수 있는 태그
    --> body, title, head 태그    ex) document.body
    
    3. window 객체
    --> Js에서 document처럼 주어지는 변수로, 열린 브라우저 창(window)에 대한 것
    ex) 열려있는 브라우저 window창에 대해서, resize 이벤트 감지
     window.addEventListener("resize", handleWindowResize);
    
    4. 여러가지 event
    resize, copy, offline, online 이벤트

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const title = document.querySelector(".hello h1:first-child")
+const h1 = document.querySelector(".hello h1:first-child")
 
 
-function handelMouseEnter() {
-    title.innerText = "Mouse is here!"
+function handleTitleClick(event) {
+    event.target.style.color = "blue";
+}
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here!"
 }
 
-function handelMouseLeave(){
-    title.innerText = "Mouse is gone!"
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!"
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato"
+}
+function handleWindowCopy(){
+    alert("copier!")
+}
+function handleWindowOffline(){
+    alert("SOS no WIFI!")
+}
+function handleWindowOnline(){
+    alert("ALL GooooD!!")
 }
 
+// 2 가지 방식으로 클릭 처리
+// h1.onclick = handleTitleClick;
+h1.addEventListener("click",handleTitleClick);
 // 마우스가 해당 element 위에 있을 때
-title.addEventListener("mouseenter", handelMouseEnter); 
-
+h1.addEventListener("mouseenter", handleMouseEnter); 
 // 마우스가 해당 element를 떠날 때
-title.addEventListener("mouseleave", handelMouseLeave);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
