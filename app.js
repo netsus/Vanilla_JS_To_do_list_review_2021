330e35a2a6a626385d79165ff58209cf9dee5ead
netsus <57648890+netsus@users.noreply.github.com>
2021-07-11 21:41:09 +0900

    #3.6 CSS in Javascript
    1. Js에서 style을 변경할 수 있지만, 분리하는 것이 좋다.
    Js에서 interactive 역할 수행, style은 CSS에서 수행
    
    2. 웹과 상호작용하는 Js의 단계
      1) element를 찾기 -> document.querySelector() 단계
      2) event를 listen -> target.addEventListener() 단계
      3) 그 event에 반응 -> addEventListener에 사용되는 함수 정의

--- app.js
+++ app.js
@@ -2,37 +2,15 @@ const h1 = document.querySelector(".hello h1:first-child")
 
 
 function handleTitleClick(event) {
-    event.target.style.color = "blue";
-}
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!"
-}
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato"
-}
-function handleWindowCopy(){
-    alert("copier!")
-}
-function handleWindowOffline(){
-    alert("SOS no WIFI!")
-}
-function handleWindowOnline(){
-    alert("ALL GooooD!!")
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue"){
+        newColor = "tomato";
+    }else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-// 2 가지 방식으로 클릭 처리
-// h1.onclick = handleTitleClick;
 h1.addEventListener("click",handleTitleClick);
-// 마우스가 해당 element 위에 있을 때
-h1.addEventListener("mouseenter", handleMouseEnter); 
-// 마우스가 해당 element를 떠날 때
-h1.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
