2e831f7d73a3b6895cd1cb2f7b4a874059fe8353
netsus <57648890+netsus@users.noreply.github.com>
2021-07-14 23:51:19 +0900

    #4.1 Form Submission
    1. app.js - 유효성 검사
    loginInput 값을 if, else if, else 문으로 유효성 검사
    --> 빈문자열 검사, 최대 길이 검사
    
    2. 위에서 app.js를 통해 한 유효성 검사를 HTML수준에서 바로 할 수 있음
    --> form태그 안에 input, button 태그를 이용
    ex) 필수값(빈문자열 검사) , 최대길이 지정
        <input
            required
            maxlength="15"
            type="text"
            placeholder="What is your name?"
          />

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
-    console.dir(loginInput);
-    console.log(loginInput.value);
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  if (username === "") {
+    alert("Please write your name");
+  } else if (username.length >= 15) {
+    alert("Your name is too long.");
+  } else {
+  }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
