bebd28796c70efbf93c510908c0696243a8f2f1c
netsus <57648890+netsus@users.noreply.github.com>
2021-08-06 01:04:06 +0900

    #6.2 Recap
    quotes.js에서 명언을 Math 라이브러리로 랜덤 인덱스로 가져옴.
    background.js에서 createElement하고 document.body에 추가
    --> prepend는 해당 요소의 자식으로 맨 위에 추가, appendChild는 맨 아래 추가
    ex) prepend, appendChild
    document.body.prepend(bgImage); // body안에서 맨 위에 추가
    document.body.appendChild(bgImage); // body안에서 맨 아래 추가

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document.body.prepend(bgImage); // body안에서 맨 위에 추가
+document.body.appendChild(bgImage); // body안에서 맨 아래 추가
