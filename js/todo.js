a2ae543f65dab91b381b3163e5fd66f9cd53cc89
netsus <57648890+netsus@users.noreply.github.com>
2021-08-08 18:29:07 +0900

    #7.1 Adding ToDos
    1. paintToDo 함수 : newTodo(스트링형태)를 입력받아 document에 li태그와 span태그를 생성하고, toDoList(ul)안에 li 넣고, li안에 span을 appendChild한 뒤에, span에 innerText로 newTodo값 넣기
    function paintToDo(newTodo) {
        const toDoList_li = document.createElement("li"); // li 태그 생성
        const span = document.createElement("span");   // span 태그 생성
        toDoList_li.appendChild(span);                             // li안에 span 넣기
        span.innerText = newTodo;                                  // span innerText 넣기
        toDoList.appendChild(toDoList_li);                      //  ul안에 li 넣기
    }
    --> handelToDoSubmit 함수 내에서 paintToDo 실행

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    const toDoList_li = document.createElement("li");
+    const span = document.createElement("span");
+    toDoList_li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(toDoList_li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
