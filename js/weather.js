e28c55d74fed453692f0cbb595974980ab02a719
netsus <57648890+netsus@users.noreply.github.com>
2021-08-17 02:49:51 +0900

    #8.1 Weather API
    https://openweathermap.org/ 사이트에서 API 사용 -> 접속한 컴퓨터의 위치(도시 정보)와 날씨(기온포함)정보 가져오기
    
    1. URL을 이용해 API 정보 가져오기 (navigator.geolocation을 이용해 latitude와 logitude를 구하고 이걸 API URL에 넣는다.)
      1) URL에 값 넣기 (벡틱이랑 ${ } 이용)
        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
    
    2. fetch함수 이용해서, then을 통해 리턴값을 연결시켜서 HTML에 결과값 표시
        fetch(url)
            .then((response) => response.json())
            .then((data) => {
                const weather = document.querySelector("#weather span:first-child");
                const city = document.querySelector("#weather span:last-child");
                weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
                city.innerHTML = data.name;
            });

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "5fd2370e380366fcdab429fe4c4fa8cc";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+            city.innerHTML = data.name;
+        });
 }
 
 function onGeoError() {
